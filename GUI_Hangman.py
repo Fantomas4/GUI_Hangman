23b36894e52d2d712785c70ce00f0254ab0c9f03
Fantomas4 <stef1997s@gmail.com>
2017-10-07 02:52:09 +0300

    Initial project commit.

---
+++ GUI_Hangman.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
+#from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+
+class WelcomeScreen(Screen):
+    pass
+
+class LoginScreen(Screen):
+    pass
+
+class MenuScreen(Screen):
+    pass
+
+class ScreenManagement(ScreenManager):
+    pass
+
+presentation = Builder.load_file("main.kv")
+
+class MainApp(App):
+    def build(self):
+        return presentation
+
+if __name__ == "__main__":
+    MainApp().run()
