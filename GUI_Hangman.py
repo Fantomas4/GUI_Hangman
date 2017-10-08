ab9a215bb17195420113c5b2a8b8b05c1780358e
Fantomas4 <stef1997s@gmail.com>
2017-10-08 04:15:49 +0300

    Added functionality to username TextInput field of LoginScreen. It now returns the typed username to login_func() upon pressing the "Enter" button.

--- GUI_Hangman.py
+++ GUI_Hangman.py
@@ -1,14 +1,21 @@
+############################ GUI PART ##########################################################
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, WipeTransition
 #from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
 
 class WelcomeScreen(Screen):
     pass
 
 class LoginScreen(Screen):
     pass
+    login_username_text_input = ObjectProperty()
+    login_password_text_input = ObjectProperty()
+
+    def login_func(self):
+        print("DIAG: username is: ", self.login_username_text_input.text)
 
 class MenuScreen(Screen):
     pass
@@ -21,6 +28,11 @@ presentation = Builder.load_file("main.kv")
 class MainApp(App):
     def build(self):
         return presentation
+#################################################################################################
+########################## BASE APP PART ########################################################
+
+
+
 
 if __name__ == "__main__":
     MainApp().run()
